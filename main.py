3cc0a0a6915da52d89788e7c967a7f1f69a5a1e1
arina <arina@MacBook-2.local>
2021-10-12 17:43:52 +0300

    Add has_letter() feature

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 
 
 class Word:
-    pass
+    def __init__(self, word: str) -> None:
+        self.word = word
+
+    def has_letter(self, letter) -> bool:
+        if isinstance(letter, int):
+            raise ValueError(f"<{letter}> is not a 'str' type")
+
+        if letter.isdigit():
+            raise ValueError(f"<{letter}> provided as 'str' type, but it's 'int' value")
+
+        return letter in self.word
 
 class Game:
     pass
