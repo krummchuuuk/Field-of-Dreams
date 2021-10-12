8b06c42d03350b16b9e58eca3e8625ab3aac418d
arina <arina@MacBook-2.local>
2021-10-12 17:20:09 +0300

    Add test for checking that word contains a provided letter

--- test_main.py
+++ test_main.py
@@ -4,8 +4,14 @@ from main import Game, Word
 
 class TestWord(unittest.TestCase):
     def setUp(self) -> None:
-        self.word = Word()
+        word = "bread"
+        self.word = Word(word)
 
+    def test_word_has_provided_letter(self):
+        self.assertEqual(self.word.has_letter("r"), True)
+        self.assertRaises(self.word.has_letter("3"), ValueError)
+        self.assertRaises(self.word.has_letter(45), ValueError)
+        self.assertEqual(self.word.has_letter("j"), False)
 
 class TestGame(unittest.TestCase):
     def setUp(self) -> None:
